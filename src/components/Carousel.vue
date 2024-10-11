8b4a6a44291314b27298146de95369e4b7866bd3
sky <50047674+skytokox@users.noreply.github.com>
2024-10-11 20:40:50 +0200

    duzy update

---
+++ src/components/Carousel.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { ref, shallowReactive } from 'vue';
+import {
+    VueFlux,
+    FluxCaption,
+    FluxControls,
+    FluxIndex,
+    FluxPagination,
+    FluxPreloader,
+    Img,
+    Fade,
+    Slide,
+    Swipe,
+} from 'vue-flux';
+import 'vue-flux/style.css';
+const options = shallowReactive({
+    autoplay: true,
+    height: '400px',
+});
+const rscs = shallowReactive([
+    new Img('/images/img1.jpg', 'img 1'),
+    new Img('/images/img2.jpg', 'img 1'),
+    new Img('/images/img3.jpg', 'img 1'),
+]);
+
+const transitions = shallowReactive([Swipe]);
+</script>
+
+<template>
+    <div class="w-full h-2/3 relative">
+        <VueFlux :options="options" :rscs="rscs" :transitions="transitions" ref="$vueFlux" class="h-full">
+
+            <template #preloader="preloaderProps">
+                <FluxPreloader v-bind="preloaderProps" />
+            </template>
+<!-- 
+            <template #caption="captionProps">
+                <FluxCaption v-bind="captionProps" />
+            </template> -->
+
+            <template #controls="controlsProps">
+                <FluxControls v-bind="controlsProps" />
+            </template>
+<!-- 
+            <template #pagination="paginationProps">
+                <FluxPagination v-bind="paginationProps" />
+            </template> -->
+
+            <!-- <template #index="indexProps">
+                <FluxIndex v-bind="indexProps" />
+            </template> -->
+        </VueFlux>
+        <div class="absolute text-white w-1/2 h-1/3 top-1/3 left-36 z-20 flex flex-col gap-4">
+            <p class="text-h1 font-bold">GABINET <br> PODOLOGICZNY</p>
+            <p class="text-h4">Renata Duda-Błasiak Sebastian Błasiak</p>
+        </div>
+    </div>
+</template>
---
+++ tailwind.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			'violet': '#A6566B',
+			'green': '#9ABF8F',
+			'gray': '#656F8C',
+			'light-green': '#9ABF8F',
+			'light-violet': '#A6566B',
+			'light-gray': '#656F8C',
+			'white': '#FFFFFF',
+			'black': '#000000',
+			'creamy': '#FAF1F0'
+		},
+		fontSize: {
+			'p': '1rem',
+			'h6': '1.2rem',
+			'h5': '1.44rem',
+			'h4': '1.728rem',
+			'h3': '2.074rem',
+			'h2': '2.488rem',
+			'h1': '2.986rem'
+		},
+		extend: {},
+	},
+	plugins: [],
+}
