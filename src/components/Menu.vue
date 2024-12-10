870e0942a5b6fdced648feab18cca82b6318d53e
sky <50047674+skytokox@users.noreply.github.com>
2024-12-11 00:32:08 +0100

    even smaller fix

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref, watch } from 'vue' 
+import { onMounted, ref, watch } from 'vue'
 const isOpened = ref(false)
 
 
@@ -17,10 +17,10 @@ watch(isOpened, () => {
 
     <div class="absolute w-full h-fit bg-white top-24 z-30 border-t-[1px] border-gray" v-if="isOpened">
         <div class="w-full flex h-12 items-end border-b-[1px] border-gray">
-            <p class="text-p pb-1"><a @click="isOpened = !isOpened" href="/#tiles">Oferta</a></p>
+            <p class="text-p pb-1"><a @click="isOpened = !isOpened" href="/#gallery">Galeria</a></p>
         </div>
         <div class="w-full flex h-12 items-end border-b-[1px] border-gray">
-            <p class="text-p pb-1"><a @click="isOpened = !isOpened" href="/#gallery">Galeria</a></p>
+            <p class="text-p pb-1"><a @click="isOpened = !isOpened" href="/#tiles">Oferta</a></p>
         </div>
         <div class="w-full flex h-12 items-end border-b-[1px] border-gray">
             <p class="text-p pb-1"><a @click="isOpened = !isOpened" href="/#about">O nas</a></p>
